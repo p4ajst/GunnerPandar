5a88f199291dcd9eb4fc3647ac97748b79278523
TSITCL\s152147 <aslhkymgam2316@gmail.com>
2018-01-11 10:01:02 +0900

    アイテムの追加

---
+++ Assets/Item.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+public var item : gameObject;
+public var nikuman : gameObject;
+public var seiro : gameObject;
+
+function Start () 
+{
+    seiro = item.transform.Find("seiro").gameObject;
+}
+
+function Update ()
+{
+    if(Input.GetKeyDown("A"))
+    {
+        if(seiro.activeSelf == true)
+        {
+            seiro.setActive(false);
+        }
+        else
+        {
+            seiro.setActive(true);
+        }
+    }
+}
